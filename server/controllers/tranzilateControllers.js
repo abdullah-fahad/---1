8f82ddeb01656d225e92a462d30b4d96165cf54c
abdullah-fahad <72708185+abdullah-fahad@users.noreply.github.com>
2020-10-23 11:15:26 +0300

    Update tranzilateControllers.js

--- server/controllers/tranzilateControllers.js
+++ server/controllers/tranzilateControllers.js
@@ -1,17 +1,26 @@
-import Source from '../models/index';
+import source from '../models/source';
 
 export const tran = async(req, res) => {
     const {code} = req.body;
+    const findOne = (x) => {
+        for (var y = 0; y <= source.arabic_code.length; y++)
+        {
+            if (x === source.arabic_code[y]){
+                return y;
+            }
+        }
+    }
+
     try{
         const codeArray = code.split(' ');
-        
         let tranz = '';
         for (var x = 0; x<codeArray.length; x++){
-            let tranzilation = await Source.findOne({where: {arabic_code: codeArray[x]}});
-            if(!tranzilation){
+             const find = findOne(codeArray[x]);
+            if(!find){
              tranz = `${tranz + codeArray[x]}`
-        }  else{
-            tranz = `${tranz + tranzilation.javaScript_code} `;
+        }  else if(find){
+            console.log(find);
+            tranz = `${tranz + source.javaScript_code[find]} `;
         }
         };
         res.status(200).json({message:tranz});
