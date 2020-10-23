e20c7f8250aac797c4d4939db7870892b23c94b1
abdullah-fahad <72708185+abdullah-fahad@users.noreply.github.com>
2020-10-23 11:13:31 +0300

    Update addController.js

--- server/controllers/addController.js
+++ server/controllers/addController.js
@@ -1,13 +1,17 @@
-import Source from '../models/index';
+import source from '../models/source';
 export const add = async(req, res) => {
-    const {arabic_code, javaScript_code, } = req.body;
+    const {arabic, javaScript, } = req.body;
+    var x = false;
     try{
-        const x = await Source.findOne({where: {arabic_code, javaScript_code}});
-        if(x) res.status(401).json({message: "الكود الذي أدخلته موجود مسبقاً"});
-         Source.create({
-            arabic_code,
-            javaScript_code,
+          
+         await source.arabic_code.forEach(element => {
+            source.arabic_code[element] === arabic? x = true: x = false; 
         });
+        if(x) res.status(401).json({message: "الكود الذي أدخلته موجود مسبقاً"});
+        
+         source.arabic_code.push(arabic);
+         source.javaScript_code.push(javaScript);
+
     res.status(200).json({message: "تم إضافة الكود إلى قاعدة البيانات"})
     }catch(e){
         res.status(500).json(e.message);
